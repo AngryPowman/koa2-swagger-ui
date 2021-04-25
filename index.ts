bf7777d11483e98d55cc9ce283e55111c6a6f1c2
AngryPowman <powmanx2@gmail.com>
2021-04-25 14:51:05 +0800

    feat: swagger API integration

--- index.ts
+++ index.ts
@@ -1,7 +1,33 @@
+import { SwaggerAPI } from "koa-joi-router-docs";
+import { Router } from "koa-joi-router";
 import { koaSwagger, KoaSwaggerUiOptions } from "./koa2-swagger";
-
+type BaseSpec = {
+  info: {
+    title: string;
+    version: string;
+    description: string;
+  };
+  tags: Array<{
+    name: string;
+    description: string;
+  }>;
+  basePath: string;
+};
 export class Koa2SwaggerUIComponent {
-  boost(config: Partial<KoaSwaggerUiOptions>) {
-    return koaSwagger(config);
+  static boost(
+    router: Router,
+    config: {
+      swaggerOptions: Partial<KoaSwaggerUiOptions>;
+      baseSpec: BaseSpec;
+    }
+  ) {
+    let swagger = new SwaggerAPI();
+    const spec = swagger.generateSpec(config.baseSpec, {
+      defaultResponses: {},
+    }) as Record<string, unknown>;
+
+    swagger.addJoiRouter(router);
+
+    return koaSwagger({ ...config, swaggerOptions: { spec } });
   }
 }
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,23 @@
 /// <reference types="koa" />
 /// <reference types="koa-compose" />
+import { Router } from "koa-joi-router";
 import { KoaSwaggerUiOptions } from "./koa2-swagger";
+declare type BaseSpec = {
+    info: {
+        title: string;
+        version: string;
+        description: string;
+    };
+    tags: Array<{
+        name: string;
+        description: string;
+    }>;
+    basePath: string;
+};
 export declare class Koa2SwaggerUIComponent {
-    boost(config: Partial<KoaSwaggerUiOptions>): import("koa-compose").Middleware<import("koa").ParameterizedContext<import("koa").DefaultState, import("koa").DefaultContext, any>>;
+    static boost(router: Router, config: {
+        swaggerOptions: Partial<KoaSwaggerUiOptions>;
+        baseSpec: BaseSpec;
+    }): import("koa-compose").Middleware<import("koa").ParameterizedContext<import("koa").DefaultState, import("koa").DefaultContext, any>>;
 }
+export {};
